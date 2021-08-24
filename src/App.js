b87c1573b8bc86d28b7f8cdd9a12606ecf9175fd
박종익 <pji3504@naver.com>
2021-08-24 22:00:26 +0900

    컴포넌트분해

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
 import React, {Component} from 'react';
 import './App.css';
+import TOC from './components/TOC'
+import Subject from './components/Subject'
+import Content from './components/Content'
+
+
 
 class App extends Component{
   render(){
     return (
       <div className="App">
-        Hello, React!!
+        <Subject title="web" sub="world wide web"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
