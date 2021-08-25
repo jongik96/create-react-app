4179f475022ebd38b732d18a8dd4d1479af201ae
박종익 <pji3504@naver.com>
2021-08-25 21:40:45 +0900

    CRUD

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,13 +1,53 @@
 import React, {Component} from 'react';
 
 class TOC extends Component{
-    render(){
+  shouldComponentUpdate(newProps, newState){
+    console.log('===>shouldComponentUpdate',
+    newProps.data,
+    this.props.data
+    )
+    if(this.props.data === newProps.data){
+      return false;
+    }
+    return true;
+
+  }
+  render(){
+      console.log('====>TOC render')
+        var lists = [];
+        var data = this.props.data
+        var i = 0;
+        while(i< data.length){
+            lists.push(
+              <li key={data[i].id}>
+                  <a
+                   href={"/content/"+data[i].id}
+                   data-id={data[i].id}
+
+                   // 속성 이용하는방법
+                  //  onClick={function(e){
+                     
+                  //    e.preventDefault();
+                  //    this.props.onChangePage(e.target.dataset.id);
+                  //  }.bind(this)}
+
+                  // 속성을 이용하지 않는 방법
+
+                   onClick={function(id, e){
+                     
+                    e.preventDefault();
+                    this.props.onChangePage(id);
+                  }.bind(this, data[i].id)}
+
+                   >{data[i].title}
+                  </a>
+              </li>)
+            i+=1
+        }
       return (
         <nav>
           <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
+            {lists}
           </ul>
         </nav>
       )
